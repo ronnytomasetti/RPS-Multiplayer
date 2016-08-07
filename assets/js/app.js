19965f37a5f9d9532fd3fe57dbff527bcf1bf4ee
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-08-07 14:36:51 -0400

    Began implementing html structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,9 +13,7 @@
  */
 
 // TODO: Create player object
-// TODO: 
 
-// Initialize Firebase
 var config = {
 	apiKey: "AIzaSyDkXBvYGeQz45NI9wQmWy6C_cS7P4KR354",
 	authDomain: "rpsls-5141f.firebaseapp.com",
@@ -23,3 +21,38 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// '.info/connected' is a special location provided by Firebase that is updated every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+var connectionsRef = database.ref("/connections");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+	// If they are connected..
+	if( snap.val() ) {
+
+		// Add user to the connections list.
+		var con = connectionsRef.push(true);
+
+		// Remove user from the connection list when they disconnect.
+		con.onDisconnect().remove();
+
+	};
+
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+
+	// Display the viewer count in the html.
+	// The number of online users is the number of children in the connections list.
+	$("#watchers").html(snap.numChildren());
+
+});
