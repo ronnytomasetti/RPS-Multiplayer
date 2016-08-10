911ff6c527fd007527e8305f3047d04925a0a990
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-08-10 06:22:30 -0400

    Started putting html and logic pieces together.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,23 +2,19 @@
  * HOMEWORK ASSIGNMENT RPS-MULTIPLAYER
  * UCF CODING BOOTCAMP 2016
  * RONNY TOMASETTI
- *
- * Instructions:
- *   1. Only two users can play at the same time.
- *   2. Both players pick either rock, paper, or scissors.
- *   3. After both players make their selection, the game will tell them the outcome (win, loss, draw).
- *   4. Game will track each player's wins, losses, and draws.
- *   5. Implement chat functionality into the game.
- *   6. Theme and style.
  */
 
- /**
-  * RPSLS Global Game Variables
-  * @type {Array}   WEAPON_CHOICES   -Store default hand choices.
-  * @type {Object}  PLAYER_OPPONENT  -Store reference to opponent object.
-  */
- var WEAPON_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
- var PLAYER_OPPONENT;
+// Instructions: ================================================================
+	// 1. Only two users can play at the same time.
+	// 2. Both players pick either rock, paper, or scissors.
+	// 3. After both players make their selection, the game will tell them the outcome (win, loss, draw).
+	// 4. Game will track each player's wins, losses, and draws.
+	// 5. Implement chat functionality into the game.
+	// 6. Theme and style.
+// ===============================================================================
+
+var WEAPON_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+var PLAYER_OPPONENT;
 
 var config = {
 	apiKey: "AIzaSyDkXBvYGeQz45NI9wQmWy6C_cS7P4KR354",
@@ -26,7 +22,6 @@ var config = {
 	databaseURL: "https://rpsls-5141f.firebaseio.com",
 	storageBucket: "rpsls-5141f.appspot.com",
 };
-
 firebase.initializeApp(config);
 
 var database = firebase.database();
